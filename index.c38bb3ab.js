f5bb36db000c63de1cc28b8e46ba6faa7a109c23
MatiushechkoMark <mmatiushechko@gmail.com>
2024-06-12 23:15:53 +0300

    make build

---
+++ index.c38bb3ab.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var a=e.target;if("IMG"===a.tagName&&(a=a.parentElement),"A"===a.tagName){var n=a.href;t.src=n}});
+//# sourceMappingURL=index.c38bb3ab.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const thumbs = document.getElementById("thumbs");
+const largeImg = document.getElementById("largeImg");
+thumbs.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    let target = evt.target;
+    if (target.tagName === "IMG") target = target.parentElement;
+    if (target.tagName === "A") {
+        const newSrc = target.href;
+        largeImg.src = newSrc;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
